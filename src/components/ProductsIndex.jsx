dffe254dbda0ff07859b649538286cb575a6f252
Derek McGee <dmcgee2001@gmail.com>
2023-11-30 12:48:48 -0500

    whoops forgot init. products index component is now made

---
+++ src/components/ProductsIndex.jsx
@@ -0,0 +1,14 @@
+export function ProductsIndex(props) {
+  return (
+    <div>
+      <h1>All products</h1>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+          <p>{product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
